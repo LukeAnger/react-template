2ed66ffb96a3fdf1355f4154d314829778915d96
LukeAnger <anger.luke@gmail.com>
2023-02-07 22:21:13 -0500

    react template initial commit with syzygy logo attached

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import SyzygyLogo from './components/SyzygyLogo.jsx'
+
+
+const App = () => {
+
+
+
+  return (
+    <section id='app'>
+      <SyzygyLogo />
+    </section>
+  )
+}
+
+export default App
