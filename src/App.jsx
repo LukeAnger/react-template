b3037dc2987e04155d7e9a00a0748ebfb7f513eb
LukeAnger <anger.luke@gmail.com>
2023-03-19 14:28:50 -0400

    removed unnecessary stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React, {useState} from 'react'
 import SyzygyLogo from './components/SyzygyLogo.jsx'
 
-
 const App = () => {
-
-
 
   return (
     <section id='app'>
-      <SyzygyLogo />
+      Hi there!
     </section>
   )
 }
